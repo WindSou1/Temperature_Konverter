fa4b90b1f4b4c41f1ad5f5c36831e57b200f7e3e
WindSou1 <31696158+WindSou1@users.noreply.github.com>
2018-01-25 01:56:07 +0300

    Update konvert.c

--- konvert.c
+++ konvert.c
@@ -7,16 +7,16 @@ int main(int argc, char *argv[])
     if(argc == 3)
         grad = *argv[2];
     temp = atof(argv[1]);
-    if(argc == 2 || grad == 'C' || grad == 'c')
+    if(argc == 2 || grad == 'C' || grad == 'c' || !grad)
     {
         if(argc == 2)
             printf("%.2f C:\n", temp);
         if(temp <= -273.15)
             printf("Low than 0");
         else
-            printf("%.2f F\t%.2f K\n\n", 32.0 + temp*9.0/5.0, temp + 273.15);
+            printf("%.2f F\n%.2f K\n\n", 32.0 + temp*9.0/5.0, temp + 273.15);
     }
-    if(argc == 2 || grad == 'F' || grad == 'f')
+    if(argc == 2 || grad == 'F' || grad == 'f' || !grad)
     {
     if(argc == 2)
         printf("%.2f F:\n", temp);
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
     else
         printf("%.2f C\n%.2f K\n\n", (temp - 32.0)*5.0/9.0, (temp - 32.0)*5.0/9.0 + 273.15);
     }
-    if(argc == 2 || grad == 'K' || grad == 'k')
+    if(argc == 2 || grad == 'K' || grad == 'k' || !grad)
     {
         if(argc == 2)
             printf("%.2f K:\n", temp);
